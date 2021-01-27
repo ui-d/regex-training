a82fd6e54c7cbef4b3ff7dadf550f77d8887f256
ui-d <dawiddeveloper@gmail.com>
2021-01-27 23:10:04 +0100

    regex basics

--- regex.js
+++ regex.js
@@ -1,1 +1,18 @@
-let regex;
+// matching
+
+let str = "This is testing of this conteent";
+
+let exp = /this/gi;
+
+let arrOfMatches = str.match(exp);
+
+let match1 = arrOfMatches[0]; // This
+let match2 = arrOfMatches[1]; // this
+
+// one or more
+
+let letterE = /e+/g;
+
+let findEs = str.match(letterE);
+
+findEs;
